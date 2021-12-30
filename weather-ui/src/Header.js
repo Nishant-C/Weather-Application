b766cd9b61603bc4d96f3eb7473857b999ac74b9
Nishant Chaturvedi <nishant.kvd@gmail.com>
2021-12-29 19:24:31 -0800

    Added a new UI project

---
+++ weather-ui/src/Header.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+class Header extends React.Component{
+
+  render(){
+
+    return (
+
+      <header> 
+        <button> Search</button>
+        <button> About</button>
+      </header>
+    )
+  }
+}
+
+export default Header
